3cf7c86411f6d0c7dc4d5aef443765799849f7ce
anant20592 <31096723+anant20592@users.noreply.github.com>
2023-07-04 23:21:38 +0530

    added intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,68 @@
 import * as React from 'react';
+import { InfiniteScroll } from './InfiniteScroll';
 import './style.css';
 
+export interface ListItemProps {
+  title: string;
+}
+
+interface ListProps {
+  list: ListItemProps[];
+}
+
 export default function App() {
+  const [value, setValue] = React.useState<string>('');
+  const [list, setList] = React.useState<ListItemProps[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const getData = (query: string, pageNumber: number) => {
+    fetch(
+      'https://openlibrary.org/search.json?' +
+        new URLSearchParams({ q: query, page: pageNumber.toString() }),
+      {
+        method: 'GET',
+      }
+    ).then(async (data) => {
+      try {
+        const listData = await data.json();
+        console.log(listData);
+        setList([...list, ...listData.docs]);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  };
+
+  const renderItem = React.useCallback((props, ref) => {
+    return <div ref={ref}>{props?.title}</div>;
+  }, []) ;
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Infinite Scroll</h1>
+      <div>
+        <input type="text" onChange={handleChange} value={value} />
+      </div>
+      <div>
+        <InfiniteScroll
+          query={value}
+          next={(query: string, page: number) => getData(query, page)}
+          listItem={renderItem}
+          data={list}
+        >
+          {/* <ul>
+            {list.map((item) => (
+              <li>
+                <p>{item.title}</p>
+              </li>
+            ))}
+          </ul> */}
+        </InfiniteScroll>
+        {(props, ref) => renderItem(props, ref)}
+      </div>
     </div>
   );
 }
--- src/InfiniteScroll.tsx
+++ src/InfiniteScroll.tsx
@@ -1,26 +1,56 @@
+import * as React from 'react';
 interface InfiniteScrollProps {
-  list: any[];
-  next: () => void;
-  hasMore: boolean;
+  query: string;
+  next: (query: string, pageNumber: number) => void;
+  hasMore?: boolean;
   children: any;
+  listItem: any;
+  data: any[];
 }
-const InfiniteScroll = ({
-  list,
+export const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
   hasMore = true,
   next,
   children,
+  query,
+  listItem,
+  data,
 }: InfiniteScrollProps) => {
+  const pageNumber = React.useRef(1);
+  const observer = React.useRef(null);
+  const lastItemRef = React.useCallback((node) => {
+    if (observer.current) return observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        pageNumber.current += 1;
+        fetchMore();
+      }
+      if (node) observer.current.observe(node);
+    });
+  }, []);
   const fetchMore = () => {
     if (hasMore) {
-      next();
+      next(query, pageNumber.current);
     }
   };
+
+  React.useEffect(() => {
+    if (query.trim() !== '') next(query, pageNumber.current);
+  }, [query]);
+
+  const renderList = () => {
+    return data.map((el, indx) => {
+      if (indx === data.length - 1) {
+        return listItem({ title: el.title }, lastItemRef);
+      } else {
+        return listItem(el, null);
+      }
+    });
+  };
+
   return (
     <>
-      {children}
-      <button onClick={fetchMore}>Fetch more</button>
+      {renderList()}
       {!hasMore && <p>No more items</p>}
     </>
   );
 };
-//https://openlibrary.org/search.json?q=spy&page=1
